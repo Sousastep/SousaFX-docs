f0d3dd43b3828177a65dca2ca35052c2be8afbc4
jbaylies <jbaylies@gmail.com>
2024-04-05 03:00:42 -0400

    update version number to 0.9.4

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ html_show_copyright = False
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.9.3'
+release = '0.9.4'
 
 
 # -- General configuration ---------------------------------------------------
