2385ff462ed2456092ecb8d2d033290439ad3043
jbaylies <jbaylies@gmail.com>
2024-08-26 17:16:32 -0400

    update version

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ html_show_copyright = False
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.9.4'
+release = '0.10.1'
 
 
 # -- General configuration ---------------------------------------------------
